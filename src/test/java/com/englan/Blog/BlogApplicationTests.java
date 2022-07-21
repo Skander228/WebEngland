b90c6d1de8aa8c7ace20d26154a4be145de73f24
Skander228 <kkhemiriskander@gmail.com>
2022-07-21 16:56:18 +0300

    Initial commit

---
+++ src/test/java/com/englan/Blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.englan.Blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
