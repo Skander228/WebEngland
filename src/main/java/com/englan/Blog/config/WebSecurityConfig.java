10b2567a965dd348963eb7a583cedef65fb9df11
Skander228 <kkhemiriskander@gmail.com>
2022-07-24 23:37:01 +0300

    Форма авторизации add

--- src/main/java/com/englan/Blog/config/WebSecurityConfig.java
+++ src/main/java/com/englan/Blog/config/WebSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.englan.Blog.config;
 
-import com.englan.Blog.repo.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
